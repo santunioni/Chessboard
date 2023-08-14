ba1913e61f599fbbe05b78149f98b214980f0485
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-08-13 21:19:47 -0300

    chore: transform to record

--- app/src/main/java/chess/board/path/BoardPath.java
+++ app/src/main/java/chess/board/path/BoardPath.java
@@ -5,14 +5,7 @@ import chess.board.position.Position;
 import java.util.Iterator;
 
 
-public class BoardPath implements Iterable<Position> {
-    final Position position;
-    final BoardPathOrientation orientation;
-
-    public BoardPath(Position position, BoardPathOrientation orientation) {
-        this.position = position;
-        this.orientation = orientation;
-    }
+public record BoardPath(Position position, BoardPathOrientation orientation) implements Iterable<Position> {
 
     public Iterator<Position> iterator() {
         return new BoardPathIterator(this.position, this.orientation);
