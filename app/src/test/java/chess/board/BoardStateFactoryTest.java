c7159be1d433e2701ec8e23434795b8f9dca834a
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-08-14 23:10:07 -0300

    test: board state initializer

--- app/src/test/java/chess/board/BoardStateFactoryTest.java
+++ app/src/test/java/chess/board/BoardStateFactoryTest.java
@@ -1,11 +1,14 @@
 package chess.board;
 
-import chess.pieces.King;
-import chess.pieces.Queen;
+import chess.board.position.File;
+import chess.board.position.Position;
+import chess.board.position.Rank;
+import chess.pieces.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardStateFactoryTest {
     @Test
@@ -43,5 +46,107 @@ public class BoardStateFactoryTest {
 
         assertInstanceOf(King.class, piece);
         assertEquals(Color.BLACK, piece.getColor());
+    }
+
+    @Test
+    void shouldPlaceWhiteRooksOnA1AndH1() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var position : List.of(new Position("a1"), new Position("h1"))) {
+            var piece = (Rook) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Rook.class, piece);
+            assertEquals(Color.WHITE, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceBlackRooksOnA8AndH8() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var position : List.of(new Position("a8"), new Position("h8"))) {
+            var piece = (Rook) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Rook.class, piece);
+            assertEquals(Color.BLACK, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceWhiteKnightsOnB1AndG1() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var position : List.of(new Position("b1"), new Position("g1"))) {
+            var piece = (Knight) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Knight.class, piece);
+            assertEquals(Color.WHITE, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceBlackKnightsOnB8AndG8() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var position : List.of(new Position("b8"), new Position("g8"))) {
+            var piece = (Knight) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Knight.class, piece);
+            assertEquals(Color.BLACK, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceWhiteBishopsOnC1AndF1() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var position : List.of(new Position("c1"), new Position("f1"))) {
+            var piece = (Bishop) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Bishop.class, piece);
+            assertEquals(Color.WHITE, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceBlackBishopsOnC8AndF8() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var position : List.of(new Position("c8"), new Position("f8"))) {
+            var piece = (Bishop) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Bishop.class, piece);
+            assertEquals(Color.BLACK, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceWhitePawnsOnSecondRank() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var file : File.values()) {
+            var position = new Position(file, Rank.TWO);
+            var piece = (Pawn) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Pawn.class, piece);
+            assertEquals(Color.WHITE, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceBlackPawnsOnSeventhRank() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var file : File.values()) {
+            var position = new Position(file, Rank.SEVEN);
+            var piece = (Pawn) board.getPieceAt(position).orElseThrow();
+            assertInstanceOf(Pawn.class, piece);
+            assertEquals(Color.BLACK, piece.getColor());
+        }
+    }
+
+    @Test
+    void shouldPlaceEmptySquaresOnThirdToSixthRank() {
+        var board = new BoardStateFactory().createFreshBoardState();
+
+        for (var file : File.values()) {
+            for (var rank : List.of(Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX)) {
+                var position = new Position(file, rank);
+                assertTrue(board.getPieceAt(position).isEmpty());
+            }
+        }
     }
 }
