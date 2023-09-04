c3da7dc44d91d3bc6f45416c5a7b9649151774cc
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-09-03 22:30:28 -0300

    fix: formatting

--- app/src/main/java/chess/game/board/BoardController.java
+++ app/src/main/java/chess/game/board/BoardController.java
@@ -18,34 +18,36 @@ import java.util.stream.Collectors;
  * The only way that ui should interact with the chess game.
  */
 public class BoardController {
-    private final BoardState boardState;
-    private final BoardHistory boardHistory;
+  private final BoardState boardState;
+  private final BoardHistory boardHistory;
 
-    public BoardController(BoardState boardState, BoardHistory boardHistory) {
-        this.boardState = boardState;
-        this.boardHistory = boardHistory;
-    }
+  public BoardController(BoardState boardState, BoardHistory boardHistory) {
+    this.boardState = boardState;
+    this.boardHistory = boardHistory;
+  }
 
-    public Optional<PieceProperties> getPieceAt(Position position) {
-        Optional<Piece> pieceOptional = this.boardState.getPieceAt(position);
-        if (pieceOptional.isPresent()) {
-            var piece = pieceOptional.get();
-            return Optional.of(piece.toProperties());
-        }
-        return Optional.empty();
+  public Optional<PieceProperties> getPieceAt(Position position) {
+    Optional<Piece> pieceOptional = this.boardState.getPieceAt(position);
+    if (pieceOptional.isPresent()) {
+      var piece = pieceOptional.get();
+      return Optional.of(piece.toProperties());
     }
+    return Optional.empty();
+  }
 
-    public List<PlayDto> getPlaysFor(Position position) {
-        var playingPieceOptional = this.boardState.getPieceAt(position);
-        return playingPieceOptional.map(piece -> piece.getPlays(this.boardState, this.boardHistory)).orElse(new HashSet<>()).stream().map(Play::toDto).collect(Collectors.toList());
-    }
+  public List<PlayDto> getPlaysFor(Position position) {
+    var playingPieceOptional = this.boardState.getPieceAt(position);
+    return playingPieceOptional.map(piece -> piece.getPlays(this.boardState, this.boardHistory))
+        .orElse(new HashSet<>()).stream().map(Play::toDto).collect(Collectors.toList());
+  }
 
-    public void makePlay(PlayDto playDto) throws PlayValidationError, IlegalPlay {
-        this.makePlay(new PlayDtoToPlayMapper(this.boardState).createPlayFromDto(playDto));
-    }
+  public void makePlay(PlayDto playDto) throws PlayValidationError, IlegalPlay {
+    this.makePlay(new PlayDtoToPlayMapper(this.boardState).createPlayFromDto(playDto));
+  }
 
-    public void makePlay(Play play) throws PlayValidationError, IlegalPlay {
-        PlayValidatorAgainstAllChessRules.validateNextPlay(this.boardState.copy(), this.boardHistory.copy(), play);
-        play.actOn(this.boardState, this.boardHistory);
-    }
+  public void makePlay(Play play) throws PlayValidationError, IlegalPlay {
+    PlayValidatorAgainstAllChessRules.validateNextPlay(this.boardState.copy(),
+        this.boardHistory.copy(), play);
+    play.actOn(this.boardState, this.boardHistory);
+  }
 }
--- app/src/test/java/chess/pieces/KnightMoveTest.java
+++ app/src/test/java/chess/pieces/KnightMoveTest.java
@@ -1,5 +1,7 @@
 package chess.pieces;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import chess.game.board.BoardHistory;
 import chess.game.board.BoardState;
 import chess.game.grid.Position;
@@ -7,59 +9,56 @@ import chess.game.pieces.Color;
 import chess.game.pieces.Knight;
 import chess.game.pieces.Pawn;
 import chess.game.plays.Move;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class KnightMoveTest {
 
-    private BoardState board;
-    private BoardHistory history;
+  private BoardState board;
+  private BoardHistory history;
 
 
-    @BeforeEach
-    void setUp() {
-        this.board = new BoardState();
-        this.history = new BoardHistory();
-    }
+  @BeforeEach
+  void setUp() {
+    this.board = new BoardState();
+    this.history = new BoardHistory();
+  }
 
-    @Test
-    void shouldMoveInL() {
-        var knight = new Knight(Color.WHITE);
-        this.board.placePiece("d4", knight);
+  @Test
+  void shouldMoveInL() {
+    var knight = new Knight(Color.WHITE);
+    this.board.placePiece("d4", knight);
 
-        var expectedValidMoves = Set.of(
-                new Move(Color.WHITE, new Position("d4"), new Position("e6")),
-                new Move(Color.WHITE, new Position("d4"), new Position("c6")),
+    var expectedValidMoves = Set.of(
+        new Move(Color.WHITE, new Position("d4"), new Position("e6")),
+        new Move(Color.WHITE, new Position("d4"), new Position("c6")),
 
-                new Move(Color.WHITE, new Position("d4"), new Position("b5")),
-                new Move(Color.WHITE, new Position("d4"), new Position("b3")),
+        new Move(Color.WHITE, new Position("d4"), new Position("b5")),
+        new Move(Color.WHITE, new Position("d4"), new Position("b3")),
 
-                new Move(Color.WHITE, new Position("d4"), new Position("c2")),
-                new Move(Color.WHITE, new Position("d4"), new Position("e2")),
+        new Move(Color.WHITE, new Position("d4"), new Position("c2")),
+        new Move(Color.WHITE, new Position("d4"), new Position("e2")),
 
-                new Move(Color.WHITE, new Position("d4"), new Position("f3")),
-                new Move(Color.WHITE, new Position("d4"), new Position("f5"))
-        );
+        new Move(Color.WHITE, new Position("d4"), new Position("f3")),
+        new Move(Color.WHITE, new Position("d4"), new Position("f5"))
+    );
 
-        assertEquals(expectedValidMoves, knight.getPlays(board, history));
-    }
+    assertEquals(expectedValidMoves, knight.getPlays(board, history));
+  }
 
-    @Test
-    void shouldBeBlockedByItsTeamMates() {
-        var knight = new Knight(Color.WHITE);
-        this.board.placePiece("b1", knight);
-        this.board.placePiece("c3", new Pawn(Color.WHITE));
+  @Test
+  void shouldBeBlockedByItsTeamMates() {
+    var knight = new Knight(Color.WHITE);
+    this.board.placePiece("b1", knight);
+    this.board.placePiece("c3", new Pawn(Color.WHITE));
 
-        var expectedValidMoves = Set.of(
-                new Move(Color.WHITE, new Position("b1"), new Position("a3")),
-                new Move(Color.WHITE, new Position("b1"), new Position("d2"))
-        );
+    var expectedValidMoves = Set.of(
+        new Move(Color.WHITE, new Position("b1"), new Position("a3")),
+        new Move(Color.WHITE, new Position("b1"), new Position("d2"))
+    );
 
-        assertEquals(expectedValidMoves, knight.getPlays(board, history));
-    }
+    assertEquals(expectedValidMoves, knight.getPlays(board, history));
+  }
 }
