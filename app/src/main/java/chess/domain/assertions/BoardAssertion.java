87c04b059845b6a5819e5d0079bf183ffcf448bc
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-10-22 16:54:47 -0300

    feat: BoardAssertion.and

--- app/src/main/java/chess/domain/assertions/BoardAssertion.java
+++ app/src/main/java/chess/domain/assertions/BoardAssertion.java
@@ -4,4 +4,8 @@ import chess.domain.board.ReadonlyBoard;
 
 public interface BoardAssertion {
   boolean test(ReadonlyBoard board);
+
+  default BoardAssertion and(BoardAssertion that) {
+    return board -> this.test(board) && that.test(board);
+  }
 }
--- app/src/main/java/chess/domain/assertions/BoardStateIsValidAssertion.java
+++ app/src/main/java/chess/domain/assertions/BoardStateIsValidAssertion.java
@@ -3,14 +3,10 @@ package chess.domain.assertions;
 import chess.domain.board.ReadonlyBoard;
 
 public class BoardStateIsValidAssertion implements BoardAssertion {
-  private final LastPlayerKingIsNotCheckedAssertion lastPlayerKingIsNotCheckedAssertion =
-      new LastPlayerKingIsNotCheckedAssertion();
-
-  private final PawnsOnFinalRankArePromotedAssertion pawnsOnFinalPositionArePromotedAssertion =
-      new PawnsOnFinalRankArePromotedAssertion();
+  private final BoardAssertion assertion = new LastPlayerKingIsNotCheckedAssertion()
+      .and(new PawnsOnFinalRankArePromotedAssertion());
 
   public boolean test(ReadonlyBoard board) {
-    return this.lastPlayerKingIsNotCheckedAssertion.test(board)
-        && this.pawnsOnFinalPositionArePromotedAssertion.test(board);
+    return this.assertion.test(board);
   }
 }
